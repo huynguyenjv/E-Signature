a4a3ae40d279b6d56e9b91c710c91efab94f07d4
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2025-07-09 00:32:18 +0700

    Initial commit

---
+++ src/main/java/java/backend/esignature/ESignatureApplication.java
@@ -0,0 +1,13 @@
+package java.backend.esignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESignatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESignatureApplication.class, args);
+    }
+
+}
