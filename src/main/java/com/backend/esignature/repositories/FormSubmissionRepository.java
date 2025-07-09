dc39dc4833b3a5cbf34d7c58ecdf21d10fa96d0b
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2025-07-10 01:11:50 +0700

    Feat: updated repository for each entity

---
+++ src/main/java/com/backend/esignature/repositories/FormSubmissionRepository.java
@@ -0,0 +1,9 @@
+package com.backend.esignature.repositories;
+
+import com.backend.esignature.entities.FormSubmissions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FormSubmissionRepository extends JpaRepository<FormSubmissions,String> {
+}
