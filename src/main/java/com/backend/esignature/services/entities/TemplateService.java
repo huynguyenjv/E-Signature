a3a2a6d54156b9d7a07423f58924022c64e304c6
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2025-07-10 01:47:37 +0700

    Feat: create entity service

---
+++ src/main/java/com/backend/esignature/services/entities/TemplateService.java
@@ -0,0 +1,9 @@
+package com.backend.esignature.services.entities;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TemplateService {
+}
