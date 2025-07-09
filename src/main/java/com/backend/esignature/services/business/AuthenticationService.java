884dd83eb6fc1acd75f3bbac5b9a3ea818a743db
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2025-07-10 01:49:23 +0700

    Feat: create business service

---
+++ src/main/java/com/backend/esignature/services/business/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.backend.esignature.services.business;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+}
