f2ec68fdd4a4d4fa915dc1ca4175822f3cf7c603
huynguyenjv <nguyenmaibaohuy2003@gmail.com>
2025-09-23 18:01:03 +0700

    feat: update kafka

---
+++ user-service/src/main/java/com/backend/userservice/kafka/UserEventConsumer.java
@@ -0,0 +1,11 @@
+package com.backend.userservice.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserEventConsumer {
+
+    private final
+}
